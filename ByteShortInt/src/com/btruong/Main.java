744cbd75f86d6b2fa66286199c874d17b2fc2d66
Bao Truong <baotruong@Baos-MacBook-Air.local>
2018-07-19 14:59:08 -0400

    First git commit

---
+++ ByteShortInt/src/com/btruong/Main.java
@@ -0,0 +1,13 @@
+package com.btruong;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myIntValue = 5;
+        float myFloatValue = 5/3f;
+        double myDoubleValue = 5.25/4d;
+        System.out.println("myIntValue =" + myIntValue);
+        System.out.println("myFloatValue =" + myFloatValue);
+        System.out.println("myDoubleValue =" +myDoubleValue);
+    }
+}
