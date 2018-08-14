5af6628b4fdfd5b2b7d2136aa0a6d519b1f9c30c
Bao Truong <baotruong@Baos-MacBook-Pro.local>
2018-08-14 16:15:28 -0400

    test

---
+++ IfKeywordAndCodeBlocks/src/com/btruong/Challenges.java
@@ -0,0 +1,18 @@
+package com.btruong;
+
+public class Challenges {
+
+    public static void main(String[] args){
+        boolean newGameOver = true;
+        int newScore = 10000;
+        int newLevelCompleted = 8;
+        int newBonus = 200;
+
+        if(gameOver) {
+            int finalScore = newScope + (newLevelCompleted * newBonus);
+            System.out.println("Your Final Score Was"+ finalScore);
+
+    }
+}
+
+
--- IfKeywordAndCodeBlocks/src/com/btruong/Main.java
+++ IfKeywordAndCodeBlocks/src/com/btruong/Main.java
@@ -1,10 +1,32 @@
-package com.btruong;
+//package com.btruong;
 
-public class Main {
+//public class Main {
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//        boolean gameOver = true;
+//        int score = 5000;
+//        int levelCompleted = 5;
+//        int bonus = 100;
 
-        boolean gameOver = true;
+//        if(score < 5000 && score > 1000){
+//            System.out.println("Your Score Was Less Than 5000");
+//
+//        } else if (score < 1000){
+//            System.out.println("Your Score Was Less Than 1000");
+//
+//        } else {
+//            System.out.println("Got Here");
+//        }
 
-    }
-}
+//        if (gameOver)== true {
+//            int finalScore = score + (levelCompleted * bonus);
+//            System.out.println("Your final score was" + finalScore);
+//        }
+//
+//
+//
+//
+//    }
+//}
+
